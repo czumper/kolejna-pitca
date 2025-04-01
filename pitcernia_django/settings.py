e36a39d611d16200ea9e610894c046cd3636e928
InstancjaBaza <kfranek45@gmail.com>
2025-04-01 20:40:53 +0000

    male poprawki

--- pitcernia_django/settings.py
+++ pitcernia_django/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS',default='localhost', cast=lambda v: [s.strip() for s in v.split(',')])
 
