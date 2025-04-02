276b93e025983e5e0c74f1609cec0aa55e351c94
czumper <kfranek44@gmail.com>
2025-04-02 14:45:14 +0200

    ss

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://pitcernia.ninja/api";
 
 // Create an axios instance
 const api = axios.create({
