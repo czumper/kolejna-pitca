032cbf3fd96a37827e1e8c76ef2a7eaa15c24be9
czumper <kfranek44@gmail.com>
2025-05-09 10:10:46 +0200

    ss

--- pitcernia_django/settings.py
+++ pitcernia_django/settings.py
@@ -146,7 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 
 CORS_ALLOWED_ORIGINS = [
-    "https://pitcernia.ninja
+    "https://pitcernia.ninja"
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
